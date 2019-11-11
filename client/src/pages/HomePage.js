39a632512772d75ed41f6848a0ed37cdfd71803a
jegors jeskovs <jegorsj@gmail.com>
2019-11-11 16:37:36 -0700

    finished the page

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import "./HomePage.css";
+import { Redirect } from 'react-router-dom'
+
+const picPosition = styled.section`
+
+    #mainLogo {
+        height: 300px; 
+        position: absolute;
+        top: 50%;
+        left: 50%;
+    }
+`
+// making it a class type component in order to use Redirect component
+class HomePage extends React.Component {
+
+    state = {
+        redirect: false
+    }
+        
+
+    
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
+    }
+
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to="/projects" />
+        }
+    }
+    
+    render () {
+        return (
+        <picPosition>
+            <div className="logoContainer">
+            {this.renderRedirect()}
+                <img onClick={this.setRedirect} id="mainLogo" src="./img/logo2.jpg"/>
+            </div>
+        </picPosition>
+        
+        
+        )
+    }
+}
+
+export default HomePage;
