f8c1587dbdbdbf0a038c5273dc57fe5848523588
Tetiana Cary <taniushka.tata@gmail.com>
2019-11-11 16:40:21 -0700

    Got the details to show in the correct space

---
+++ controllers/detailsController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+// Defining methods for the projectsController
+module.exports = {
+    findAll: function (req, res) {
+        db.Detail
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function (req, res) {
+        db.Detail
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function (req, res) {
+        db.Detail
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.Detail
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function (req, res) {
+        db.Detail
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};
