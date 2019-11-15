e0f212f2f68559ed8b81308eb6fceacc064cd371
Tetiana Cary <taniushka.tata@gmail.com>
2019-11-15 13:54:18 -0700

    Added a readme and chenge the logo border color

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -20,7 +20,7 @@ body {
   padding: 20px;
   box-shadow: 
        inset 0 -3em 3em rgba(0,0,0,0.1), 
-             0 0  0 2px rgb(255,255,255),
+             0 0  0 2px #2a878c,
              0.3em 0.3em 1em rgba(0,0,0,0.3);
 }
 
