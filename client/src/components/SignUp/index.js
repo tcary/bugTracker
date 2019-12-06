97cf7f30e9ea486bbe3337666351aa284abe97b4
Tetiana Cary <taniushka.tata@gmail.com>
2019-12-05 18:52:38 -0700

    Deleted magnifying glass. Created 2 components for login and signup

---
+++ client/src/components/SignUp/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+class SignUp extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  };
+}
